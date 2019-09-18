da9b5597dab6d6ea4badaefdb05ee0d382ac93bf
Chris York <christopherayork1992@gmail.com>
2019-09-18 16:02:37 -0400

    Fixed errorWrapper

--- handlers/errorHandling.js
+++ handlers/errorHandling.js
@@ -1,11 +1,12 @@
-function errorWrapper(req, res, callback) {
+const errorWrapper = callback => async (...args) => {
   if(!callback) return false;
   try {
-    callback();
+    await callback(...args);
   } catch(e) {
     console.log(e);
+    const [,res] = args;
     res.status(500).json({ error: 'Could not complete request' });
   }
-}
+};
 
 module.exports = errorWrapper;
--- server.js
+++ server.js
@@ -15,48 +15,36 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors(corsConfig));
 
-server.get('/api/users', authorize, (req, res) => {
-  errorWrapper(req, res, () => {
-    userDB.find()
-      .then(r => {
-        console.log(r);
-        if(r) res.status(200).json(r);
-        else res.status(400).json({ message: 'Could not get users' });
-      });
-  });
-});
+server.get('/api/users', authorize, errorWrapper(async (req, res) => {
+  let r = await userDB.find();
+  console.log(r);
+  if(r) res.status(200).json(r);
+  else res.status(400).json({ message: 'Could not get users' });
+}));
 
-server.post('/api/register', (req, res) => {
+server.post('/api/register', errorWrapper((req, res) => {
   const user = req.body;
   if(!user || !user.username || !user.password) res.status(401).json({ message: 'You must provide a username and password' });
-  else errorWrapper(req, res, () => {
-    user.password = bcrypt.hashSync(user.password, 8);
-    userDB.insert(user)
-      .then(r => {
-        console.log('Insert Result: ', r);
-        r = r[0];
-        if(r) res.status(201).json({ message: 'User created', user_id: r });
-        else res.status(400).json({ message: 'Failed to create user' });
-      });
-  });
-});
+  user.password = bcrypt.hashSync(user.password, 8);
+  userDB.insert(user)
+    .then(([r]) => {
+      console.log('Insert Result: ', r);
+      if(r) res.status(201).json({ message: 'User created', user_id: r });
+      else res.status(400).json({ message: 'Failed to create user' });
+    });
+}));
 
-server.post('/api/login', (req, res) => {
+server.post('/api/login', errorWrapper(async (req, res) => {
   const user = req.body;
   if(!user || !user.username || !user.password) res.status(401).json({ message: 'You must provide a username and password' });
-  else errorWrapper(req, res, () => {
-    userDB.findByName(user.username)
-      .then(storedUser => {
-        console.log(storedUser);
-        storedUser = storedUser[0];
-        if(storedUser && bcrypt.compareSync(user.password, storedUser.password)) {
-          const token = generateToken(storedUser);
-          res.status(200).json({ message: `Welcome ${storedUser.username}!`, token });
-        }
-        else res.status(400).json({ message: 'You shall not pass!' });
-      });
-  });
-});
+  let [storedUser] = await userDB.findByName(user.username);
+  console.log(storedUser);
+  if(storedUser && await bcrypt.compare(user.password, storedUser.password)) {
+    const token = generateToken(storedUser);
+    res.status(200).json({ message: `Welcome ${storedUser.username}!`, token });
+  }
+  else res.status(400).json({ message: 'You shall not pass!' });
+}));
 
 
 
